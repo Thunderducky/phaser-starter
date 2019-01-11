6602dc689de4f01bf45d8ea3879b209293590861
Eric Scott <scottzillaster@gmail.com>
2019-01-11 11:54:48 -0600

    beginning of the client app

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require("webpack");
+
+module.exports = {
+  entry: {
+    app: './client/src/index.js'
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'client/src/'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  },
+
+
+  devServer: {
+    contentBase: path.resolve(__dirname, 'build')
+  },
+
+  plugins: [
+    new CopyWebpackPlugin([{
+      from: path.resolve(__dirname, 'client/index.html'),
+      to: path.resolve(__dirname, 'build')
+    }]),
+    new webpack.DefinePlugin({
+      'typeof CANVAS_RENDERER': JSON.stringify(true),
+      'typeof WEBGL_RENDERER': JSON.stringify(true),
+    })
+  ],
+  mode: "development"
+};
